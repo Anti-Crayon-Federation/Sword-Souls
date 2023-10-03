769e5f28b19cfce34deaf853050ddcfd35b95ab6
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2023-10-03 05:43:38 +0200

    Updated Player Detection
    Some Stuff has been done on the detection side of things
    
    Need to add  some options to turn the shit off ( Visuals Only For Debug )

--- Assets/Scripts/PlayerDetection.cs
+++ Assets/Scripts/PlayerDetection.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
 {
+    // Player Object
     public string playerTag = "Player";
+    // Detection Indicator
+    public Color highlightColor = Color.yellow; 
+    private Color originalColor; 
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Dont "fuc***g" use this 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
@@ -23,14 +31,17 @@ public class PlayerDetection : MonoBehaviour
         if (other.gameObject.CompareTag(playerTag))
         {
             Debug.Log("Player has entered the area");
+            spriteRenderer.color = highlightColor;
         }
     }
 
+    // This function is called when another object exits the trigger collider
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag(playerTag))
         {
             Debug.Log("Player has left the area");
+            spriteRenderer.color = originalColor;
         }
     }
 }
