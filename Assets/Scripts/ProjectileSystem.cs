4f82dc2a4301851ec8dbeb34605c084854eb80aa
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2023-10-27 07:49:58 +0100

    Projectile System
    Just working some movement

--- Assets/Scripts/ProjectileSystem.cs
+++ Assets/Scripts/ProjectileSystem.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ProjectileSystem : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,16 @@ public class ProjectileSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
     }
 }
