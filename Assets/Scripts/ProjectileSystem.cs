521fabc95115a72bfe6ae5d111807b6fb5d3bffd
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2023-10-29 18:48:09 +0100

    Projectile System Updates
    "Relax"

--- Assets/Scripts/ProjectileSystem.cs
+++ Assets/Scripts/ProjectileSystem.cs
@@ -7,6 +7,11 @@ public class ProjectileSystem : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
 
+    [SerializeField]
+    private float maxDetectionDistance = 10f;
+    // The CircleCollider2D that represents the detection radius
+    public CircleCollider2D detectionRadius; 
+
     private float timer;
     private GameObject player;
     // Start is called before the first frame update
@@ -18,12 +23,13 @@ public class ProjectileSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
         float distance = Vector2.Distance(transform.position, player.transform.position);
         Debug.Log(distance);
 
-        if(distance < 10)
+        // Set the radius of the CircleCollider2D to the distance value
+        detectionRadius.radius = Mathf.Min(distance, maxDetectionDistance);
+
+        if (distance < maxDetectionDistance)
         {
             timer += Time.deltaTime;
             if (timer > 2)
